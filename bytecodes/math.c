4675d12125dc664bc15b676872b6399b4834b13d
yusufhaji7 <yususfhadji873@gmail.com>
2022-10-06 04:14:39 -0700

    bytecodes

---
+++ bytecodes/math.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * nop - does nothing
+ * @inst: pointer to head of a list
+ * @line_number: bytecode line number
+ */
+void nop(stact_t **inst, unsigned int line_number)
+{
+	(void) inst;
+	(void) line_number;
+}
